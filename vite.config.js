b0dc5ea9f6cf35ad5160f065a8557b72c8708c30
Parag <sparagshendkar@gmail.com>
2024-09-03 08:39:59 +0530

    working properly

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/random_password_generator/', // Set the base path to your repo name
 })
